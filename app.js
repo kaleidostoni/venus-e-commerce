24f2f317ec591c7104414c806d7decbc46f64409
kaleidostoni <kaleidostoni@gmail.com>
2018-03-12 13:25:15 -0600

    agregando request

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
 
 
-$( document ).ready(function(){$(".button-collapse").sideNav();})
+$(document).ready(function () {
+    $(".button-collapse").sideNav();
+})
+
+const cardigansURL = `https://openapi.etsy.com/v2/listings/active?keywords=CARDIGAN%20KNIT%20WOMAN&includes=Images:1&api_key=llkjywrb9bbj142bo4qbp1t5`
+const dressesURL = `https://openapi.etsy.com/v2/listings/active?keywords=dress%20for%20women&includes=Images:1&api_key=llkjywrb9bbj142bo4qbp1t5`
+const blousesULR = `https://openapi.etsy.com/v2/listings/active?keywords=blouse&includes=Images:1&api_key=llkjywrb9bbj142bo4qbp1t5`
+const underwaerURL = `https://openapi.etsy.com/v2/listings/active?keywords=bralette&includes=Images:1&api_key=llkjywrb9bbj142bo4qbp1t5`
+const tShirtsURL = `https://cors-anywhere.herokuapp.com/https://openapi.etsy.com/v2/listings/active?keywords=womens%20graphic%20tees&includes=Images:1&api_key=llkjywrb9bbj142bo4qbp1t5`
+const pantsURL = `https://openapi.etsy.com/v2/listings/active?keywords=WOMANS+JEANS&includes=Images:1&api_key=llkjywrb9bbj142bo4qbp1t5`
+
+//función para pintar poducto
+
+function handleResponse(data) {
+    let container = document.getElementById('container');
+    console.log(container);
+    let results = data.results;
+    results.forEach(product => {
+      let template = 
+    })
+}
+
+//petición api etsy
+const peticionEtsy = () => {
+    const url = `https://cors-anywhere.herokuapp.com/https://openapi.etsy.com/v2/listings/active?keywords=womens%20graphic%20tees&includes=Images:1&api_key=llkjywrb9bbj142bo4qbp1t5`
+    fetch(url)
+        .then(response => response.json()).then(json => handleResponse(json));
 
 
+}
+
+peticionEtsy();

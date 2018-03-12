1b749125ad8dd0b10142281a5bb888971647ab71
velvetcerise <velvet.cerise@hotmail.com>
2018-03-12 13:11:03 -0600

    prueba firebase

---
+++ login.js
@@ -0,0 +1,20 @@
+//login con firebase
+var provider = new firebase.auth.GoogleAuthProvider();
+$('#login').click(function(){
+    firebase.auth()
+    .signInWithPopup(provider).then(function(result) {
+    });
+        console.log(result.user);
+        $('#login')
+});
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCKrr1DaJn5pk7YYccD1itP5FDlxgXerIE",
+    authDomain: "venus-ecommerce.firebaseapp.com",
+    databaseURL: "https://venus-ecommerce.firebaseio.com",
+    projectId: "venus-ecommerce",
+    storageBucket: "venus-ecommerce.appspot.com",
+    messagingSenderId: "829622324466"
+  };
+  firebase.initializeApp(config);

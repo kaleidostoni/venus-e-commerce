dbf3fdca82603363e2085636d49eca2c8107e422
velvetcerise <velvet.cerise@hotmail.com>
2018-03-13 10:12:05 -0600

    autenticación ok

--- login.js
+++ login.js
@@ -1,19 +1,52 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCKrr1DaJn5pk7YYccD1itP5FDlxgXerIE",
+  authDomain: "venus-ecommerce.firebaseapp.com",
+  databaseURL: "https://venus-ecommerce.firebaseio.com",
+  projectId: "venus-ecommerce",
+  storageBucket: "venus-ecommerce.appspot.com",
+  messagingSenderId: "829622324466"
+};
+firebase.initializeApp(config);
+
 //login con firebase
 var provider = new firebase.auth.GoogleAuthProvider();
+
 $('#login').click(function(){
-    firebase.auth()
-    .signInWithPopup(provider).then(function(result) {
+  firebase.auth()
+  .signInWithPopup(provider)
+  .then(function(result) {
       console.log(result.user);
-        $('#login')});
+      save(result.user);
+      $('#login').hide();
+      $('#pseudo').append('<img src="'+result.user.photoURL+'"/>')
+    });
+    
+});
+//función que guardo los datos automaticamente
+function save(user) {
+var user = {
+  uid:user.uid,
+  nombre:user.displayName,
+  email:user.email,
+  foto:user.photoURL
+}
+firebase.database().ref('venus/' + user.iud) //guarda en rama siempre que el usuario inicie sesión
+.set(user)
+}
+
+//guardar en base de datos
+$('#pseudo').click(function () {
+firebase.database().ref('venus')
+.set({
+  nombre: 'BlisS',
+  edad: '15',
+})        
 });
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCKrr1DaJn5pk7YYccD1itP5FDlxgXerIE",
-    authDomain: "venus-ecommerce.firebaseapp.com",
-    databaseURL: "https://venus-ecommerce.firebaseio.com",
-    projectId: "venus-ecommerce",
-    storageBucket: "venus-ecommerce.appspot.com",
-    messagingSenderId: "829622324466"
-  };
-  firebase.initializeApp(config);
+//Base de datos
+firebase.database().ref('venus')
+.on('child_added', function(s){
+var user = s.val();
+$('#pseudo').append('<img width="50vh" src= "'+user.foto+'"/>');  
+})

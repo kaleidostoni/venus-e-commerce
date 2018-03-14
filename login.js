a980cf9843087dc3d34b6fb4ca5f4488737836d9
velvetcerise <velvet.cerise@hotmail.com>
2018-03-14 10:38:11 -0600

    autenticación OK- v2

--- login.js
+++ login.js
@@ -1,14 +1,43 @@
-//login con firebase
 var provider = new firebase.auth.GoogleAuthProvider();
-   
+
 $('#login').click(function(){
     firebase.auth()
     .signInWithPopup(provider)
     .then(function(result) {
-     });
-     console.log(result.user);
-    //     $('#login').hide();
-    //     $().append('<img src'"+result.user.photoURL+"'/>')
+        console.log(result.user);
+        save(result.user);
+        $('#login').hide();
+        $('#pseudo').innerHTML = "";
+        
+        $('#pseudo').append('<img width="50vh" vspace=10vh; src="'+result.user.photoURL+'" />')
+      });
+      
+});
+//función que guardo los datos automaticamente
+function save(user) {
+  var user = {
+    uid:user.uid,
+    nombre:user.displayName,
+    email:user.email,
+    foto:user.photoURL
+  }
+  firebase.database().ref('venus/' + user.iud) //guarda en rama siempre que el usuario inicie sesión
+  .set(user)
+}
+
+//guardar en base de datos
+$('#pseudo').click(function () {
+  firebase.database().ref('venus')
+  .set({
+    // nombre: 'prueba',
+    // edad: '0',
+  })        
 });
 
-  // Initialize Firebase
+// //Base de datos
+// firebase.database().ref('venus')
+// .on('child_added', function(s){
+//   var user = s.val();
+//   $('#pseudo').append('<img width="50vh" vspace=10vh; src= "'+user.foto+'"/>');  
+//   // document.getElementById('login').innerHTML = 'logout';
+// })
